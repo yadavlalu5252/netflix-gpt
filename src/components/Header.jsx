d433239ea75a0acd6cc53f9e7fe221e6657d7311
yadavlalu5252 <yadavlalu5252@gmail.com>
2025-06-28 21:42:06 +0530

    constants added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,22 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
+import { NETFLIX_LOGO, NETFLIX_USER_LOGO } from "../utils/constants";
 
 const Header = () => {
-
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const user = useSelector(store => store.user);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
 
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {})
       .catch((error) => {
         navigate("/error");
-        
       });
   };
 
-    useEffect(() => {
+  useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         const { uid, email, displayName, photoURL } = user;
@@ -44,22 +43,20 @@ const Header = () => {
   }, []);
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
-      <img
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="logo"
-        className="w-44"
-      />
-      {user && <div className="flex p-2 items-center cursor-pointer">
-        <img
-          src="https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg"
-          // src={user.photoURL}
-          alt="who's-watching-logo"
-          className="w-8 h-8 rounded-md"
-        />
-        <button onClick={handleSignOut} className="font-bold text-white">
-          Sign Out
-        </button>
-      </div>}
+      <img src={NETFLIX_LOGO} alt="logo" className="w-44" />
+      {user && (
+        <div className="flex p-2 items-center cursor-pointer">
+          <img
+            src={NETFLIX_USER_LOGO || user.photoURL}
+            // src={user.photoURL}
+            alt="who's-watching-logo"
+            className="w-8 h-8 rounded-md"
+          />
+          <button onClick={handleSignOut} className="font-bold text-white">
+            Sign Out
+          </button>
+        </div>
+      )}
     </div>
   );
 };
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+
+export const NETFLIX_LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const NETFLIX_USER_LOGO = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
