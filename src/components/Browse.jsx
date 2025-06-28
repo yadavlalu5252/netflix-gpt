0248ed7ab7439f628775008231bee20560c28a2b
yadavlalu5252 <yadavlalu5252@gmail.com>
2025-06-28 22:20:46 +0530

    tmdb api setup done and configuration in project done

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
 import Header from "./Header";
 
 const Browse = () => {
-    return (
-        <div>
-            <Header />
-        </div>
-    )
-}
+  const getNowPlayingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/now_playing?language=en-IN&page=1",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json.results);
+  };
+  useEffect(()=> {
+    getNowPlayingMovies();
+  },[]);
+
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
 
 export default Browse;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,10 @@
 
 export const NETFLIX_LOGO = "https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c0-0104-4e96-a291-092c11350111/01938dc4-59b3-7bbc-b635-c4131030e85f/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const NETFLIX_USER_LOGO = "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
+export const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzg3NWIyYjA5ODMxM2RjZDAzNWVjZjBlMWJlNTc3MSIsIm5iZiI6MTc1MTEyNzM2OS44LCJzdWIiOiI2ODYwMTU0OWE4ZGEyYzg2MzJhNGQwYTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.X0Ztm6HF1BwIGW1EVSCMr-K3rCuWEBP7Y3luJF2lJrU'
+  }
+};
